cb268cf5b51e8e4d1339e3ccb31b0c6a0358103f
Danil Belov <d.belov2@g.nsu.ru>
2025-05-21 15:36:40 +0700

    redesign

--- life-cycles/src/Components/HeaderPage/Profile/Profile.js
+++ life-cycles/src/Components/HeaderPage/Profile/Profile.js
@@ -2,12 +2,15 @@ import styles from "./Profile.module.css";
 import accountImage from "../../../Images/AccountCircle.png";
 import { useDispatch } from 'react-redux';
 import { logout } from '../../../Store/Slices/UserSlice';
+import { useNavigate } from 'react-router-dom';
 
 export function Profile() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(logout());
+    navigate('/'); // Перенаправление на страницу логина (корневой путь)
   };
 
   return (
